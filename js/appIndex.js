0d180839b0859380a014a0bf311a4dd227a86c19
diego-oyarce <doyarce@utem.cl>
2021-11-25 15:42:25 -0300

    Distribución de camiones

--- js/appIndex.js
+++ js/appIndex.js
@@ -1,20 +1,22 @@
 function leerArchivo(e) {
-    var archivo = e.target.files[0];
-    if (!archivo) {
-      return;
-    }
-    var lector = new FileReader();
-    lector.onload = function(e) {
-      var contenido = e.target.result;
-      mostrarContenido(contenido);
-    };
-    lector.readAsText(archivo);
+  var archivo = e.target.files[0];
+  if (!archivo) {
+    return;
   }
+  var lector = new FileReader();
+  lector.onload = function(e) {
+    var contenido = e.target.result;
+    mostrarContenido(contenido);
+  };
+  lector.readAsText(archivo);
+  alert(contenido);
+}
   
-  function mostrarContenido(contenido) {
-    var elemento = document.getElementById('contenido-archivo');
-    elemento.innerHTML = contenido;
-  }
-  
-  document.getElementById('file-input')
-    .addEventListener('change', leerArchivo, false);
+function mostrarContenido(contenido) {
+  var elemento = document.getElementById('contenido-archivo');
+  elemento.innerHTML = contenido;
+}
+
+document.getElementById('file-input')
+  .addEventListener('change', leerArchivo, false);
+
