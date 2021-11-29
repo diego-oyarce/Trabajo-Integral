9719b087532c84327c3952a5d605c0266dac7a79
diego-oyarce <doyarce@utem.cl>
2021-11-29 20:24:18 -0300

    Avance funciones de validación de archivos

--- js/appIndex.js
+++ js/appIndex.js
@@ -1,3 +1,9 @@
+const { number } = require("mathjs");
+import {number} from 'mathjs'
+
+var arrayPuntosDeVenta = new String(); //número identificador contenido en el archivo de parametros
+var arrayCentrosDeDistribución = new String(); ////número identificador contenido en el archivo de parametros
+
 function leerArchivo(e) {
   var archivo = e.target.files[0];
   if (!archivo) {
@@ -8,7 +14,14 @@ function leerArchivo(e) {
     var contenido = e.target.result;
     var arrayParametros = new Array();
     arrayParametros = separarDatos(contenido);
-    mostrarContenido(contenido);
+    if(estructuraArchivo(arrayParametros)){
+      mostrarContenido(contenido);
+    }
+    else{
+      alert("El formato del archivo ingresado es incorrecto.");
+      return false;
+    }
+    
   };
   lector.readAsText(archivo);
 }
@@ -26,25 +39,63 @@ function separarDatos(contenido){
   var newstr = contenido.replace(/\n/g,";");
   newstr2 = newstr.replace(/\s/g,"");
   arrayAux = newstr2.split(";");
+  //alert(arrayAux[]);
   return arrayAux;
 }
 
 function estructuraArchivo(arrayParametros){
   if((arrayParametros.lenght % 4) == 0){
-    verificarLetras(arrayParametros);
+    verificarVentaODistribucion(arrayParametros);
     return true;
   }
   else{
-    alert("Los datos ingresados son incorrectos.");
+    alert("El formato del archivo ingresado es incorrecto.");
     return false;
   }
 }
 
-function verificarLetras(arrayParametros){
-  for(let i=0; i<arrayParametros.lenght; (i+4)){
+function verificarVentaODistribucion(arrayParametros){
+  for(let i=0; i<arrayParametros.lenght; (i+3)){
     let letra = arrayParametros[i];
     if((letra != 'P') || (letra != 'C')){
-      
+      alert("El formato del archivo ingresado es incorrecto.");
+      return false;
+    }
+    else{
+      if(verificarIdentificador(arrayParametros)){
+        return true;
+      }
+      else{
+        alert("El formato del archivo ingresado es incorrecto.");
+        return false;
+      }
+    }
+  }
+}
+
+function verificarIdentificador(arrayParametros){
+  for(let i=1; i<arrayParametros.lenght; (i+3)){
+    if(typeof(arrayParametros[i]) == 'number'){
+      verificarCoordenadas(arrayParametros);
+      return true;
+    }
+    else{
+      alert("El formato del archivo ingresado es incorrecto.");
+      return false;
+    }
+  }
+}
+
+function verificarCoordenadas(arrayParametros){
+  let coordenadas = new Array();
+  for(let i=2; i<arrayParametros.lenght; (i+3)){
+    coordenadas = arrayParametros[i].split(",");
+    if((typeof(coordenadas[0]) == "number") && (typeof(coordenadas[1] == "number"))){
+      return true;
+    }
+    else{
+      alert("El formato del archivo ingresado es incorrecto.");
+      return false;
     }
   }
 }
